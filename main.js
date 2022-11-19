cbb6ecea7b340e4b981ea137914a95ba3366550d
rodrigo.thirteen <rodrigoncalves.net@hotmail.com~>
2022-11-19 20:51:57 -0300

    versão desktop

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ function createGame(player1, hour, player2) {
   `
 }
 
+let delay = -0.3
 function createCard(date, day, games) {
+  delay = delay + 0.3
   return `
-  <div class="card">
+  <div class="card" style="animation-delay: ${delay}s">
           <h2>${date} <span>${day} </span></h2>
 
           <ul>
@@ -20,22 +22,7 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
- <header>
-        <img src="./assets/logo.svg" alt="Logo da NLW" />
-      </header>
-
-      <main id="cards"> 
-      ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
-      ${createCard(
-        "28/11",
-        "segunda",
-        createGame("brazil", "13:00", "switzerland")
-      )} 
-      ${createCard(
-        "02/12",
-        "sexta",
-        createGame("cameroon", "16:00", "brazil")
-      )} 
-      </main>
-`
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland")) +
+  createCard("02/12", "sexta", createGame("cameroon", "16:00", "brazil"))
